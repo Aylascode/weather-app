344b4a51520935492e3e8f634beeca77870d2469
Aylascode <69477764+Aylascode@users.noreply.github.com>
2020-10-06 21:52:57 +0100

    updated current day weather

--- src/index.js
+++ src/index.js
@@ -15,43 +15,75 @@ function getDayAndTime(event) {
   if (minutes < 10) minutes = `0${minutes}`;
   let fullTime = `${hours}:${minutes}`;
   let dayAndTime = `${day} ${fullTime}`;
-
   return `${dayAndTime}`;
 }
 
 function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#city");
-  let cityContent = document.querySelector("#citySearch");
-
-  searchInput.innerHTML = cityContent.value;
+  let city = document.querySelector("#citySearch").value;
+  city = city.trim();
+  city = city.toLowerCase();
+  cityLocation(city);
 }
 
-function fahrenheit(event) {
-  event.preventDefault();
-  let temperatureF = document.querySelector("#temperature");
-  temperatureF.innerHTML = 66;
+function cityLocation(city) {
+  let apiKey = "65f06ea937ff0ead217cbd4698c89acf";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}`).then(showLocationWeather);
 }
 
-function celsius(event) {
-  event.preventDefault();
-  let temperatureF = document.querySelector("#temperature");
-  temperatureF.innerHTML = 19;
+function showLocationWeather(responce) {
+  document.querySelector(".temperature").innerHTML = Math.round(
+    responce.data.main.temp
+  );
+  document.querySelector("#city").innerHTML = responce.data.name;
+  document.querySelector("#description").innerHTML =
+    responce.data.weather[0].description;
+  document.querySelector("#humidity-today").innerHTML =
+    responce.data.main.humidity;
+  document.querySelector("#wind-today").innerHTML = Math.round(
+    responce.data.wind.speed
+  );
 }
 
-//Feature 1
+function callMyCurrentPosition(position) {
+  let units = "metric";
+  let apiKey = `d6f748e993063f854666ff2303ac05d1`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}`).then(showLocationWeather);
+}
+
+function geolocation() {
+  navigator.geolocation.getCurrentPosition(callMyCurrentPosition);
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", geolocation);
+
+let citySearchBar = document.querySelector("#searchBar");
+citySearchBar.addEventListener("submit", search);
+
 let todayTime = document.querySelector("#todayAndTime");
 let now = new Date();
 todayTime.innerHTML = getDayAndTime(now);
 
-//Feature 2
-let cityForm = document.querySelector("#searchBar");
-cityForm.addEventListener("submit", search);
+cityLocation("Birmingham");
 
-//Challenge 3
+// function fahrenheit(event) {
+//   event.preventDefault();
+//   let temperatureF = document.querySelector("#temperature");
+//   temperatureF.innerHTML = 66;
+// }
 
-let fahrenheitChangeTemp = document.querySelector("#fahrenheit-link");
-fahrenheitChangeTemp.addEventListener("click", fahrenheit);
+// function celsius(event) {
+//   event.preventDefault();
+//   let temperatureF = document.querySelector("#temperature");
+//   temperatureF.innerHTML = 19;
+// }
 
-let celsiusChangeTemp = document.querySelector("#celsius-link");
-celsiusChangeTemp.addEventListener("click", celsius);
+// let fahrenheitChangeTemp = document.querySelector("#fahrenheit-link");
+// fahrenheitChangeTemp.addEventListener("click", fahrenheit);
+
+// let celsiusChangeTemp = document.querySelector("#celsius-link");
+// celsiusChangeTemp.addEventListener("click", celsius);
